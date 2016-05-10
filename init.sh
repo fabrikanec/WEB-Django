446e94b9060e38da2bbc31012ceb799af8a9ee34
Cezar <cezar@localhost.localdomain>
2016-05-10 04:34:01 +0300

    First

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+#!/usr/bin/env bash
 
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
