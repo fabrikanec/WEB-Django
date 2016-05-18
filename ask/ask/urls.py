96b06621bef187224a871cadeff4bea3da12f076
Cezar <cezar@localhost.localdomain>
2016-05-19 01:41:41 +0300

    Third

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,13 +1,29 @@
-from django.conf.urls import patterns, include, url
+"""ask URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
 from django.contrib import admin
 
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')), 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('qa.urls')),
-)
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.my_login, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^question/(?P<id>\d+)/$', views.question, name='question'),
+    url(r'^ask/$', views.ask, name='ask'),
+    url(r'^popular/$', views.popular, name='popular'),
+    url(r'^new/$', views.index, name='new'),
+    url(r'^answer/$', views.answer, name='answer'),
+]
