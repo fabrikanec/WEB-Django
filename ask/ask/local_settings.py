d5a1d01d7dc214ec58bf4004b2f0e4ca06da9e87
Cezar <cezar@localhost.localdomain>
2016-05-19 03:17:57 +0300

    Third

--- ask/ask/local_settings.py
+++ ask/ask/local_settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'qa',
+    'autofixture',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,12 +59,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stepic',
-        'USER': 'admin',
-        'PASSWORD': 'qwerty',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
